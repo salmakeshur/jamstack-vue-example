a1c9182b1ad907b7c5ee67a81d0cbfca3c88d209
salma keshur <salma@simformsolutions.com>
2023-09-15 11:02:20 +0530

    intial code push

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Hello, JAMstack with Vue.js!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
